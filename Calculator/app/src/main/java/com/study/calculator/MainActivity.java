c8b836c0b35c1a107d4bcd1ee7c5eaec4287e6c0
ViktorHolovanets <victorgolova@gmail.com>
2023-04-16 22:14:18 +0300

    calculator

---
+++ Calculator/app/src/main/java/com/study/calculator/MainActivity.java
@@ -0,0 +1,123 @@
+package com.study.calculator;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String variable1 = "";
+    private String operation;
+    private String variable2 = "";
+    private Boolean isOneVariable = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
+//        GridLayout gridLayout = findViewById(R.id.ButtonPanel);
+//
+//        int buttonCount = gridLayout.getChildCount();
+//        for (int j = 0; j < buttonCount; j++) {
+//            View button = gridLayout.getChildAt(j);
+//            if (button instanceof Button) {
+//                Button btn = (Button) button;
+//                GridLayout.LayoutParams params = (GridLayout.LayoutParams) btn.getLayoutParams();
+//                GridLayout.Spec rowSpec = GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f);
+//                GridLayout.Spec colSpec = GridLayout.spec(GridLayout.UNDEFINED, 1, 1f);
+//                params.rowSpec = rowSpec;
+//                params.columnSpec = colSpec;
+//
+//                if (btn.getText().toString().equals("0")) {
+//                    params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 2);
+//                    params.width = GridLayout.LayoutParams.WRAP_CONTENT; // Додатковий рядок для розтягування кнопки "0"
+//                }
+//                btn.setLayoutParams(params);
+//            }
+//        }
+    }
+
+    public void onClickNumber(View view) {
+        Button btn = (Button) view;
+        String num = btn.getText().toString();
+        setTextResult(num, true);
+        if (isOneVariable) {
+            variable1 += num;
+        } else {
+            variable2 += num;
+        }
+    }
+
+    public void onClickOperation(View view) {
+       if(isOneVariable){
+           Button btn = (Button) view;
+           operation = btn.getText().toString();
+           setTextResult(operation, true);
+           isOneVariable = !isOneVariable;
+       }
+    }
+
+    private void setTextResult(String text, Boolean isAdd) {
+        TextView textView = findViewById(R.id.ResultPanel);
+        if (isAdd)
+            textView.setText(textView.getText().toString() + text);
+        else
+            textView.setText(text);
+    }
+
+    public void clearResult(View view) {
+        variable1 = "";
+        variable2 = "";
+        operation = "";
+        isOneVariable = true;
+        TextView textView = findViewById(R.id.ResultPanel);
+        textView.setText("");
+    }
+
+    public void onClickResult(View view) {
+        setTextResult(result(), false);
+        variable2 = "";
+        operation = "";
+        isOneVariable = true;
+        TextView textView = findViewById(R.id.ResultPanel);
+
+    }
+
+    private String result() {
+        try {
+            float v1 = Float.parseFloat(variable1);
+            float v2 = Float.parseFloat(variable2);
+            float result = 0;
+            switch (operation) {
+                case "+":
+                    result = v1 + v2;
+                    break;
+                case "-":
+                    result = v1 - v2;
+                    break;
+                case "*":
+                    result = v1 * v2;
+                    break;
+                case "/":
+                    if (v2 != 0)
+                        result = v1 / v2;
+                    break;
+                case "%":
+                    result = (v1 / 100) * v2;
+                    break;
+            }
+            variable1=String.valueOf(result);
+            return variable1;
+        } catch (Exception e) {
+        }
+        return "";
+    }
+}
+
